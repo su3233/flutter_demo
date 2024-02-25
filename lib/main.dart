177e1f67271e23e3a5c54132986c080988fd790e
tongsheng.su <mr_su3200@sina.com>
2024-02-25 11:23:59 +0800

    text,button,Image

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,87 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: const Text("你好flutter"),
+        title: const Text(
+          "你好flutter",
+          textAlign: TextAlign.center,
+        ),
       ),
-      body: const MyAppTwo(),
+      body: Column(
+        children: [
+          const MyAppTwo(),
+          MyButton(),
+          MyText(),
+          const SizedBox(
+            height: 20,
+          ),
+          MyImage()
+        ],
+      ),
     ),
   ));
+}
+
+/// 2、flutter图片组件Image，本地图片，远程图片，图片剪切，圆形图片
+/// Image.Asset   Image.remote
+
+class MyImage extends StatelessWidget {
+  const MyImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 150,
+          height: 150,
+          // margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+          decoration: const BoxDecoration(color: Colors.grey),
+          child: Image.network(
+            "https://pics0.baidu.com/feed/0eb30f2442a7d933f7c0526420464b1e73f00153.jpeg@f_auto?token=9f2355599b661de1b49379336ca0b621",
+            fit: BoxFit.cover,
+            // scale: 2,
+            repeat: ImageRepeat.repeatX, //背景图片平铺
+          ),
+        ),
+        Container(
+          width: 150,
+          height: 150,
+          margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+          decoration: const BoxDecoration(
+              color: Colors.grey,
+              image: DecorationImage(
+                  image: NetworkImage(
+                "https://pics0.baidu.com/feed/0eb30f2442a7d933f7c0526420464b1e73f00153.jpeg@f_auto?token=9f2355599b661de1b49379336ca0b621",
+              ))),
+        )
+      ],
+    );
+  }
+}
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 500,
+        height: 50,
+        margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+        decoration: const BoxDecoration(color: Colors.yellow),
+        child: const Text(
+          "你好FLutter你好FLutter你好FLutter你好FLutter",
+          textAlign: TextAlign.left,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+              fontSize: 20,
+              color: Colors.red,
+              fontWeight: FontWeight.w900,
+              fontStyle: FontStyle.italic,
+              letterSpacing: 2,
+              decorationStyle: TextDecorationStyle.dashed,
+              decorationColor: Colors.black,
+              decoration: TextDecoration.underline),
+        ));
+  }
 }
 
 class MyAppTwo extends StatelessWidget {
@@ -20,13 +96,49 @@ class MyAppTwo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "我是一个自定义组件",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.green, fontSize: 40),
+    return Center(
+      child: Container(
+        width: 100,
+        height: 100,
+        margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+        //位移，旋转transform
+        // transform: Matrix4.translationValues(10, 0, 0),
+        transform: Matrix4.rotationZ(0.2),
+        //旋转
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.green,
+            border: Border.all(color: Colors.red, width: 2),
+            //可以实现圆形
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [BoxShadow(color: Colors.blue, blurRadius: 20.0)],
+            gradient:
+                const LinearGradient(colors: [Colors.red, Colors.yellow])),
+        child: const Text(
+          "你好flutter",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
       ),
     );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 200,
+        height: 40,
+        // margin: const EdgeInsets.all(10),
+        margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+        // padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+        child: const Text(
+          "按钮",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ));
   }
 }
 
