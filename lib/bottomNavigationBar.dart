f239e545e02daad1a8956debb80894c0f80aa74d
sutongsheng <sutongsheng@soundai.com>
2024-03-07 12:28:13 +0800

    BottomNavigationBar

--- lib/bottomNavigationBar.dart
+++ lib/bottomNavigationBar.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,16 +9,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "flutter statefull",
+    return const MaterialApp(
+      title: "BottomNavigationBar",
       // theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text("flutter stateful"),
-        ),
-        body: Text("data"),
+      home: Tabs(),
+    );
+  }
+}
+
+class Tabs extends StatefulWidget {
+  const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() {
+    return _TabStates();
+  }
+}
+
+class _TabStates extends State<Tabs> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("BottomNavigationBar"),
       ),
+      body: const Center(
+        child: Text("data"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (v) {
+            setState(() {
+              _currentIndex = v;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
+            BottomNavigationBarItem(icon: Icon(Icons.category), label: "分类"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "设置"),
+          ]),
     );
   }
 }
