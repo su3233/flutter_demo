2b452d412b4ba9a7c26845c4ec8b80098f5cd4d3
sutongsheng <sutongsheng@soundai.com>
2024-03-07 14:49:11 +0800

    BottomNavigationBar

--- lib/bottomNavigationBar.dart
+++ lib/bottomNavigationBar.dart
@@ -22,35 +22,119 @@ class Tabs extends StatefulWidget {
 
   @override
   State<Tabs> createState() {
-    return _TabStates();
+    return _TabsState();
   }
 }
 
-class _TabStates extends State<Tabs> {
+class _TabsState extends State<Tabs> {
   int _currentIndex = 0;
+  final List<Widget> _pages = const [
+    HomePage(),
+    CategoryPage(),
+    MessagePage(),
+    SettingPage(),
+    UserPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text("BottomNavigationBar"),
-      ),
-      body: const Center(
-        child: Text("data"),
-      ),
+      appBar: AppBar(title: const Text("Flutter App")),
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Colors.red,
+          //选中的颜色
+// iconSize:35, //底部菜单大小
           currentIndex: _currentIndex,
-          onTap: (v) {
+          //第几个菜单选中
+          type: BottomNavigationBarType.fixed,
+          //如果底部有4个或者4个以上的菜单的时候就需要配置这个参数
+
+          onTap: (index) {
             setState(() {
-              _currentIndex = v;
+              _currentIndex = index;
             });
           },
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
             BottomNavigationBarItem(icon: Icon(Icons.category), label: "分类"),
+            BottomNavigationBarItem(icon: Icon(Icons.message), label: "消息"),
             BottomNavigationBarItem(icon: Icon(Icons.settings), label: "设置"),
+            BottomNavigationBarItem(icon: Icon(Icons.people), label: "用户")
           ]),
+      floatingActionButton: Container(
+        height: 60,
+        width: 60,
+        padding: const EdgeInsets.all(4),
+        margin: const EdgeInsets.only(top: 4),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(30)),
+        child: FloatingActionButton(
+          backgroundColor: _currentIndex == 2 ? Colors.red : Colors.blue,
+          onPressed: () {
+            setState(() {
+              _currentIndex = 2;
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class UserPage extends StatelessWidget {
+  const UserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("UserPage"),
+    );
+  }
+}
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("SettingPage"),
+    );
+  }
+}
+
+class MessagePage extends StatelessWidget {
+  const MessagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("MessagePage"),
+    );
+  }
+}
+
+class CategoryPage extends StatelessWidget {
+  const CategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("CategoryPage"),
+    );
+  }
+}
+
+class HomePage extends  StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("HomePage"),
     );
   }
 }
